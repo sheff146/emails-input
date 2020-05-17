4c0769bf3ecd63672f1883aabdf5d420b8fde27d
sheff146 <alexandre.shevchenko@gmail.com>
2020-05-17 16:57:55 +0200

    Add tests for e-mail

---
+++ src/email.spec.ts
@@ -0,0 +1,17 @@
+import { Email } from './email';
+
+describe('Email', () => {
+    it('should create a valid e-mail', () => {
+        const email = new Email('abc@xyz.com');
+
+        expect(email.value).toBe('abc@xyz.com');
+        expect(email.isValid).toBe(true);
+    });
+
+    it('should create an invalid e-mail', () => {
+        const email = new Email('abc');
+
+        expect(email.value).toBe('abc');
+        expect(email.isValid).toBe(false);
+    });
+});
