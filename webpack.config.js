7ab2af4d5bfdcc3dbcee4048e703cd025bf6ed65
sheff146 <alexandre.shevchenko@gmail.com>
2020-05-17 18:38:50 +0200

    Add gh-pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,76 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const nodeEnv = process.env.NODE_ENV || 'development';
-const isProd = nodeEnv === 'production';
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const nodeEnv = process.env.NODE_ENV || "development";
+const isProd = nodeEnv === "production";
 
 const plugins = [
-    new webpack.DefinePlugin({
-        'process.env': {
-            NODE_ENV: JSON.stringify(nodeEnv)
-        }
-    }),
-    new HtmlWebpackPlugin({
-        template: '!!ejs-loader!src/index.ejs'
-    }),
-    new webpack.LoaderOptionsPlugin({
-        options: {
-            tslint: {
-                emitErrors: true,
-                failOnHint: true
-            }
-        }
-    })
+  new webpack.DefinePlugin({
+    "process.env": {
+      NODE_ENV: JSON.stringify(nodeEnv)
+    }
+  }),
+  new HtmlWebpackPlugin({
+    template: "!!ejs-loader!src/index.ejs"
+  }),
+  new webpack.LoaderOptionsPlugin({
+    options: {
+      tslint: {
+        emitErrors: true,
+        failOnHint: true
+      }
+    }
+  })
 ];
 
 var config = {
-    devtool: isProd ? 'hidden-source-map' : 'source-map',
-    context: path.resolve('./src'),
-    entry: {
-        app: './index.ts'
-    },
-    output: {
-        path: path.resolve('./dist'),
-        filename: '[name].bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.tsx?$/,
-                exclude: [/\/node_modules\//],
-                use: ['awesome-typescript-loader', 'source-map-loader']
-            },
-            !isProd
-                ? {
-                    test: /\.(js|ts)$/,
-                    loader: 'istanbul-instrumenter-loader',
-                    exclude: [/\/node_modules\//],
-                    query: {
-                        esModules: true
-                    }
-                }
-                : null,
-            { test: /\.html$/, loader: 'html-loader' },
-            {
-                test: /\.css$/,
-                loaders: ['style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]']
+  devtool: isProd ? "hidden-source-map" : "source-map",
+  context: path.resolve("./src"),
+  entry: {
+    app: "./index.ts"
+  },
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.tsx?$/,
+        exclude: [/\/node_modules\//],
+        use: ["awesome-typescript-loader", "source-map-loader"]
+      },
+      !isProd
+        ? {
+            test: /\.(js|ts)$/,
+            loader: "istanbul-instrumenter-loader",
+            exclude: [/\/node_modules\//],
+            query: {
+              esModules: true
             }
-        ].filter(Boolean)
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    plugins: plugins,
-    devServer: {
-        contentBase: path.join(__dirname, 'dist/'),
-        compress: true,
-        port: 3000,
-        hot: true
-    }
+          }
+        : null,
+      { test: /\.html$/, loader: "html-loader" },
+      {
+        test: /\.css$/,
+        loaders: [
+          "style-loader",
+          "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]"
+        ]
+      }
+    ].filter(Boolean)
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  plugins: plugins,
+  devServer: {
+    contentBase: path.join(__dirname, "dist/"),
+    compress: true,
+    port: 3000,
+    hot: true
+  }
 };
 
 module.exports = config;
